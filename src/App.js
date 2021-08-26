d584451829f54d6ed31d9b2983fd7136b7eb2f6c
kareemIt <kareem.itani2@gmail.com>
2021-08-26 15:55:11 -0400

    Setting up the css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>To Do List</h1>
-      <div className="container">
+      <div className="toDoList">
         {/* add comp here later  */}
-      </div>
+        </div>
       <div className="inputInfoContainer">
         <h1>write stuff here</h1>
         <input type="text" />
