e2ac1cd385044ced117922c458542da2cf002c11
kareemIt <kareem.itani2@gmail.com>
2021-08-26 15:49:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bp54pq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bp54pq)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <div className="container">
+        {/* add comp here later  */}
+      </div>
+      <div className="inputInfoContainer">
+        <h1>write stuff here</h1>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
